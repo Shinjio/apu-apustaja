0d198eb9d9cbbbc68a20aff875c68c1a542a17e4
Shinjio <ilcaneeunanimale@gmail.com>
2020-05-19 17:53:56 +0200

    chess engine

--- addons/chess.py
+++ addons/chess.py
@@ -1,9 +1,13 @@
+import threading
 import chess
 import chess.svg
+from chess.polyglot import open_reader
+import time
 import cairosvg
 import discord
 import random
 import asyncio
+import chessai
 from discord.ext import commands
 
 
@@ -37,6 +41,29 @@ class Chess(commands.Cog):
         self.black.turn = not self.black.turn
 
     @commands.command(pass_context=True)
+    async def engine(self, ctx):
+        author = ctx.message.author
+        opponent = "engine"
+        self.white = Player('white', author)
+        self.white.turn = True
+        self.black = Player('black', "engine")
+        self.board = chess.Board()
+        self.genBoardImg()
+        await ctx.message.channel.send(file=discord.File("board.png"))
+    
+        """
+        o = 0
+        while o < 1000:
+            if o % 2 == 0:
+                await ctx.message.channel.send("Your turn. use the enginemove command")
+                move = await 
+            else:
+                move = chessai.alphaBetaRoot(4, self.board, True)
+                move = chess.Move.from_uci(str(move))
+                self.board.push(move)
+            o += 1
+        """
+    @commands.command(pass_context=True)
     async def challenge(self, ctx, username):
         author = ctx.message.author
         opponent = ctx.message.mentions[0]
@@ -49,16 +76,33 @@ class Chess(commands.Cog):
     @commands.command(pass_context=True)
     async def move(self, ctx, move = ''):
         player = self.white if self.white.turn == True else self.black
-        print(player.username)
-        print(ctx.message.author)
         if str(ctx.message.author) == str(player.username):
             try:
                 move = ctx.message.content[9:]
-                print(move)
                 self.board.push_san(move)
                 self.genBoardImg()
                 await ctx.message.channel.send(file=discord.File("board.png"))
                 self.swapTurns()
+                if self.black.username == "engine":
+                    with open_reader('gamebook/games.bin') as reader:
+                        opening_moves = [str(entry.move) for entry in reader.find_all(self.board)]
+                    if opening_moves:
+                        choice = random.randint(0, len(opening_moves) // 2)
+                        move = chess.Move.from_uci(opening_moves[choice])
+                        move = chess.Move.from_uci(str(move))
+                        self.board.push(move)
+                        self.genBoardImg()
+                        self.swapTurns()
+                        await ctx.message.channel.send(file=discord.File("board.png"))
+                    else:
+                        print("Thinking...")
+                        move = chessai.alphaBetaRoot(3,self.board,True)
+                        move = chess.Move.from_uci(str(move))
+                        self.board.push(move)
+                        self.genBoardImg()
+                        self.swapTurns()
+                        await ctx.message.channel.send(file=discord.File("board.png"))
+
 
                 if self.board.is_game_over() or self.board.is_checkmate():
                     await ctx.message.channel.send("[*] Game Over.")
@@ -71,7 +115,6 @@ class Chess(commands.Cog):
 
         else:
             await ctx.message.channel.send("Not {}'s turn!".format(ctx.message.author))
-
 
 
 def setup(bot):
